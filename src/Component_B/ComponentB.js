f3669a7765863cbd0113f68798cfbf1d3851a5cc
kodezen <kodezen@protonmail.com>
2020-05-15 12:10:44 -0400

    Implemented rxjs observable in test-service so that ComponentB can subscribe and update state. Component will re-render when state updates, properly displaying the value that is currently set in test-service.

--- src/Component_B/ComponentB.js
+++ src/Component_B/ComponentB.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import * as testService from "../services/test-service";
 
-function ComponentB() {
-    return (
-        <div>
-            <h1>Component B</h1>
-            <button onClick={() => console.log(testService.getValue())}>Console.log value</button>
-            <h2>Value is: {testService.getValue()}</h2>
-        </div>
-    );
+class ComponentB extends Component {
+
+    state = {value: testService.getValue()};
+
+    componentDidMount() {
+        testService.getValue$().subscribe(newValue => {
+            console.log("Recived a new value through the value stream");
+            console.log(newValue);
+            this.setState({value: newValue});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Component B</h1>
+                <button onClick={() => console.log(testService.getValue())}>Console.log value</button>
+                <h2>Value is: {this.state.value}</h2>
+            </div>
+        );
+    }
 }
 
 export default ComponentB;
--- src/services/test-service.js
+++ src/services/test-service.js
@@ -1,3 +1,5 @@
+import { BehaviorSubject } from "rxjs";
+
 let value = 5;
 
 export const getValue = () => {
@@ -8,10 +10,32 @@ export const getValue = () => {
 export const setValue = newValue => {
     console.log("test-service: setValue() called")
     value = newValue;
+    updateValueSubject(newValue);
 }
 
 export const increment = () => {
     console.log("test-service: increment() called")
     value += 1;
+    updateValueSubject(value);
 }
 
+///////* ADDITIONAL OBSERVABLE CODE *///////
+
+// Create a subject - the thing that will be watched by the observable. Set the initial value to 5 to match above
+let valueSubject = new BehaviorSubject(5);
+
+/* Create an observable to watch (observe) the subject and send out a stream of updates (you will subscribe
+ to this stream in a component to receive the updates) */
+let value$ = valueSubject.asObservable();
+
+/* Internal function that we'll call whenever setValue() or increment() is called for the normal value 
+variable so that our valueSubject matches up with value. 
+
+To update the subject being watched by the observable, we call the next method on the subject. 
+
+Then, because we have an observable watching the valueSubject, a new value will appear in the value$ 
+observable stream, and any component that subscribes to it will automatically receive the new value */
+const updateValueSubject = newValue => valueSubject.next(newValue);
+
+// Getter to expose the value stream to components
+export const getValue$ = () => value$;
