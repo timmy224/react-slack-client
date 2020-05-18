ce7485036af85923c551b607e66ce95b765b77d1
kodezen <kodezen@protonmail.com>
2020-05-18 18:26:25 -0400

    Finished implementing chatService and added a commented out test to App.js to ensure it's working as expected.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+// import * as chatService from "./services/chat-service"; 
+
+// Commented out code is a test of how the chatService runs
+// TODO remove eventually
+// chatService.getMessages$().subscribe(message => console.log("Message received from chatService: ", message));
+// chatService.getJoinedChat$().subscribe(username => console.log("User joined chat received from chatService: ", username));
+
+// let messages = [
+//   {sender: "Luis", time_sent: "12:01", content: "yo"},
+//   {sender: "Sleyter", time_sent: "12:01", content: "sup"},
+//   {sender: "Konrad", time_sent: "12:01", content: "greetings"}
+// ];
+
+// chatService.onMessagesReceived(messages);
+
+// chatService.onUserJoinedChat("Timmy");
+
+// let messageTypedFromClient = {
+//   sender: "Timmy", time_sent: "12:02", content: "Whazzaaap"
+// };
+
+// chatService.sendMessage(messageTypedFromClient);
 
 function App() {
   return (
