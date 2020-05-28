52be6c54fdbbe8a3af1cfb3bc807069c2c429c7e
kodezen <kodezen@protonmail.com>
2020-05-28 14:16:01 -0400

    Added store.js file which takes in an object containing all of our reducers, combines them into one root reducer, sets up redux-thunk middleware, creates our store, and returns it.

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+
+const configureStore = reducers => {
+    const rootReducer = combineReducers(reducers);
+    const middleware = applyMiddleware(thunkMiddleware);
+
+    return createStore(rootReducer, middleware);
+}
+
+export default configureStore;
