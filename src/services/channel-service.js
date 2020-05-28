7e25a0134847a68586b761c31c48ff9869d42aa7
kodezen <kodezen@protonmail.com>
2020-05-28 18:33:11 -0400

    Switched back to remote url.

--- src/services/channel-service.js
+++ src/services/channel-service.js
@@ -2,7 +2,7 @@ const ChannelService = function() {
     const fetchChannels = () => {
         let remoteUrl = "https://react-slack-server.herokuapp.com/channels";
         let localUrl = "http://localhost:5000/channels";
-        return fetch(localUrl)
+        return fetch(remoteUrl)
             .then(response => response.json())
             .then(data => JSON.parse(data.channels));
     }
