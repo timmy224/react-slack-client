3fc3a395236a89fee4acf69c6cb408dd2ac27f57
Sleyter De La Cruz <Sleyter2616@gmail.com>
2020-08-18 13:06:37 -0400

    removed check channel request and altered logic to get handeled all on one route

--- src/components/CreateChannel/CreateChannel.js
+++ src/components/CreateChannel/CreateChannel.js
@@ -22,20 +22,18 @@ class CreateChannel extends React.Component {
     handleSubmit = (event) => {
         const { channel_name, takenChannelName, changeRoute } = this.props
         event.preventDefault();
-        services.channelService.checkChannelName(channel_name)
-            .then(isAvailable => {
-                if (isAvailable) {
-                    services.channelService.createChannel(channel_name)
-                        .then(success => {
-                            changeRoute({path:"/main"})
-                        })
-                        .catch(err => console.log(err));
-                } else {
-                    takenChannelName(true);
-                }
-            })
-            .catch(err => console.log(err));
-    }
+        services.channelService.createChannel(channel_name)
+        .then(data => {
+            if (data.successful){
+                 changeRoute({path:"/main"})
+        }
+            else if(data.ERROR == "Channel name is taken"){
+                return takenChannelName
+            }
+    })
+         
+        .catch(err => console.log(err));
+                    }
 
     handleChange = (event) =>{
         let channel_name = event.target.value
