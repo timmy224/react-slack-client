16d3f378167363831c118fb063733dc4df23bba1
Sleyter De La Cruz <Sleyter2616@gmail.com>
2020-09-21 16:03:51 -0400

    added reigster css styles

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 // Depends on userService, storageService, socketService, registerService
 import { services } from "../../context";
 import { actions } from "../../context";
+import "./Register.css"
 
 const mapStateToProps = (state)=>{
     return { 
@@ -66,25 +67,31 @@ class Register extends Component {
         const missingCred = showMissingCred ?  <h3>Either password or username are missing.</h3> : null;
         return (
             <Fragment>
-                <h1>Register</h1>
+                <img className="logo" src="https://a.slack-edge.com/bv1-8/slack_logo-ebd02d1.svg"></img>
+                <h1 className="register">Register for a new account</h1>
+                <h6 className="create">Create an account with the username and password you will use to sign in.</h6>
                 {takenMessage}
                 {missingCred}
                 <input
+                    className="loginInput"
                     onChange={this.handleChangeUser}
                     type="text"
                     placeholder="Username"
                 />
                 <input 
+                    className="loginInput"
                      onChange={this.handleChangePassword} 
                      type="password"
                      placeholder="Password"
                 />
                 <input
+                className="signInReg"
                  onClick={this.handleSubmit}
                   type="submit"
-                  value="register" />
+                  value="Register" />
                 
                 <button
+                className="signInReg"
                  onClick = {()=> changeRoute({path:"/login"})}>Login Form
                  </button>      
             
