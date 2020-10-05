66c5d1e104b3d777799aaa3974b361d65a774453
Luis <lz24069@gmail.com>
2020-10-05 13:17:00 -0400

    make channel-name persist in redux state and use it for chat header

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -22,9 +22,10 @@ class Chat extends Component {
 
 
     render() {
+        let { numChannelMembers, channelName } = this.props
         let messages = this.props.messages ? this.props.messages : [];
         let chatHeader = this.props.chatType === "channel" 
-                                                ? <ChannelChatHeader numberOfUsers={this.props.numChannelMembers}/> 
+                                                ? <ChannelChatHeader numberOfUsers={numChannelMembers} channelName={channelName}/> 
                                                 : <PrivateChatHeader />
         return (
             <div>
@@ -56,7 +57,8 @@ const mapStateToProps = (state) => {
         partnerUsername: state.chat.partnerUsername,
         channel: state.chat.channel,
         currentInput: state.chat.currentInput,
-        numChannelMembers: state.channel.numChannelMembers
+        numChannelMembers: state.channel.numChannelMembers,
+        channelName: state.channel.channel_name,
     }
     const isChannelChat = mapping.chatType === "channel";
     const isPrivateChat = mapping.chatType === "private";
