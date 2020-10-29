04380c28ef9f9cfa5ec108d4e747e44bc86aef78
kodezen <kodezen@protonmail.com>
2020-10-29 09:21:15 -0400

    Added TODO's for socket-service events invited-to-org and added-to-org

--- src/services/socket-service.js
+++ src/services/socket-service.js
@@ -2,107 +2,103 @@ import io from "socket.io-client";
 import { Subject } from "rxjs";
 import { actions, store } from "../context";
 
-
 function SocketService(chatService) {
-    let socket;
-    let isConnected = false;
-    let connected$ = new Subject();
+  let socket;
+  let isConnected = false;
+  let connected$ = new Subject();
 
-    const getConnected = () => isConnected;
+  const getConnected = () => isConnected;
 
-    const getConnected$ = () => connected$;
+  const getConnected$ = () => connected$;
 
-    const connect = dataObject => {
-        let localUrl = "http://localhost:5000";
-        let remoteUrl = "https://react-slack-server.herokuapp.com";
-        let value = dataObject.username
-        let query_val = `username=${value}`
-        let queryObj = { query: query_val }
-        socket = io(localUrl, queryObj)
-        setUpEventListeners();
-        return socket
-    };
+  const connect = dataObject => {
+    let localUrl = "http://localhost:5000";
+    let remoteUrl = "https://react-slack-server.herokuapp.com";
+    let value = dataObject.username;
+    let query_val = `username=${value}`;
+    let queryObj = { query: query_val };
+    socket = io(localUrl, queryObj);
+    setUpEventListeners();
+    return socket;
+  };
 
-    const send = (event_name, obj) => {
-        // assuming socket declared when connected
-        socket.emit(event_name, obj)
-        console.log(event_name, "with", obj, "sent.")
-    };
+  const send = (event_name, obj) => {
+    // assuming socket declared when connected
+    socket.emit(event_name, obj);
+    console.log(event_name, "with", obj, "sent.");
+  };
 
-    const disconnect = () => {
-        // assuming socket declared when connected
-        socket.emit("disconnect")
-        socket.disconnect()
-        isConnected = false;
-    };
+  const disconnect = () => {
+    // assuming socket declared when connected
+    socket.emit("disconnect");
+    socket.disconnect();
+    isConnected = false;
+  };
 
-    const setUpEventListeners = () => {
-        socket.on("connect", () => {
-            isConnected = true;
-            connected$.next(true);
-        });
+  const setUpEventListeners = () => {
+    socket.on("connect", () => {
+      isConnected = true;
+      connected$.next(true);
+    });
 
-        socket.on("connect_error", () => {
-            isConnected = false;
-            connected$.next(false);
-        });
+    socket.on("connect_error", () => {
+      isConnected = false;
+      connected$.next(false);
+    });
 
-        socket.on('user-joined-chat', (user_join) => {
-            console.log("user_join", user_join);
-            console.log(`User joined the chat: ${user_join.username}`);
-            chatService.onUserJoinedChat(user_join.username);
-        });
+    socket.on("user-joined-chat", user_join => {
+      console.log("user_join", user_join);
+      console.log(`User joined the chat: ${user_join.username}`);
+      chatService.onUserJoinedChat(user_join.username);
+    });
 
-        socket.on('message-received', (message_received) => {
-            console.log("message-received: ", message_received);
-            console.log(
-                `Sender: ${message_received.sender},
+    socket.on("message-received", message_received => {
+      console.log("message-received: ", message_received);
+      console.log(
+        `Sender: ${message_received.sender},
                 Time Sent: ${message_received.time_sent},
                 Content: ${message_received.content}`
-            );
-            chatService.onMessageReceived(message_received);
-        });
-
-        socket.on("channel-deleted", (channelId) => {
-            store.dispatch(actions.channel.channelDeleted(channelId));
-        });
-
-        socket.on("channel-created", () => {
-            store.dispatch(actions.channel.fetchChannels());
-        });
-        
-        socket.on("added-to-channel", async (channelId) => {
-            console.log("added-to-channel", channelId);
-            await store.dispatch(actions.channel.fetchChannels());
-            store.dispatch(actions.message.initChannelMessages(parseInt(channelId)));
-            send("join-channel", channelId);
-        });
-
-        socket.on("permissions-updated", () => {
-            store.dispatch(actions.permission.fetchPermissions());
-        });
-
-        socket.on("invited-to-org", (orgName) => {
-            console.log("invited-to-org", orgName);
-        });
-
-        socket.on("added-to-org", (orgName) => {
-           console.log("added-to-org", orgName) ;
-        });
-    }
-
-    return Object.freeze({
-        getConnected,
-        getConnected$,
-        connect,
-        send,
-        disconnect,
+      );
+      chatService.onMessageReceived(message_received);
     });
+
+    socket.on("channel-deleted", channelId => {
+      store.dispatch(actions.channel.channelDeleted(channelId));
+    });
+
+    socket.on("channel-created", () => {
+      store.dispatch(actions.channel.fetchChannels());
+    });
+
+    socket.on("added-to-channel", async channelId => {
+      console.log("added-to-channel", channelId);
+      await store.dispatch(actions.channel.fetchChannels());
+      store.dispatch(actions.message.initChannelMessages(parseInt(channelId)));
+      send("join-channel", channelId);
+    });
+
+    socket.on("permissions-updated", () => {
+      store.dispatch(actions.permission.fetchPermissions());
+    });
+
+    socket.on("invited-to-org", orgName => {
+      console.log("invited-to-org", orgName);
+      // TODO fetch org invites
+    });
+
+    socket.on("added-to-org", orgName => {
+      console.log("added-to-org", orgName);
+      // TODO fetch orgs
+    });
+  };
+
+  return Object.freeze({
+    getConnected,
+    getConnected$,
+    connect,
+    send,
+    disconnect
+  });
 }
 
 export default SocketService;
-
-
-
-
-
