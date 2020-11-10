58fe371ba238eb893a1fabca9ed7b30d8c4768ff
kodezen <kodezen@protonmail.com>
2020-11-09 21:55:45 -0500

    Started setting up orgs component

---
+++ src/components/Orgs/Orgs.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+const mapStateToProps = {
+    
+}
+
+const mapActionsToProps = {
+
+}
+
+class Orgs extends Component {
+
+    selectOrg = (event) => {
+        
+    }
+
+    render() {
+        return (
+            <div class="orgs">
+                
+            </div>
+        );
+    }
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(Orgs);
