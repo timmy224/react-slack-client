cc6a0e140892614a8557630cdab0fa3344368bc7
kodezen <kodezen@protonmail.com>
2020-11-10 15:47:06 -0500

    Got PrivateChatHeader working, only adding channel name to mapping if in channel chat

--- src/components/ChatHeader/PrivateChatHeader.js
+++ src/components/ChatHeader/PrivateChatHeader.js
@@ -8,7 +8,7 @@ function PrivateChatHeader(props){
 	}
 	return(
 		<div className = "chat-header" >
-			<h1>{`# ${props.channelName}`}</h1>
+			<h1>{`# ${props.partnerUsername}`}</h1>
 			<button 
 			type="button" 
             className="btn btn-light custom-button">
