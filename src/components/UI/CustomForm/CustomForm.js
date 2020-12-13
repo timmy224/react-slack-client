0c5650ef00ff69bbc972f7db3af1ea7c47889049
Luis <lz24069@gmail.com>
2020-12-13 13:03:14 -0500

    delete console logs

--- src/components/UI/CustomForm/CustomForm.js
+++ src/components/UI/CustomForm/CustomForm.js
@@ -3,7 +3,6 @@ import React from 'react';
 import styles from './CustomForm.module.css'
 
 const CustomForm = ({children, ...otherProps}) =>{
-    console.log({children})
     return(
         <form className={styles.customForm} {...otherProps}>
             {children}
