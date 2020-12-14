835af86a7d7dc451ecf8feab6545d2d400037d7a
kodezen <kodezen@protonmail.com>
2020-12-14 11:02:37 -0500

    - removed FORMAT from datetime-service - removed dateTimeService.getDtStr() - added MESSAGE_DT_FORMAT to chat-service - added DISPLAY_MESSAGE_DT_FORMAT to message component and getting RAW_MESSAGE_DT_FORMAT from services.chatService.MESSAGE_DT_FORMAT - now destructuring props object in Message component - Removed unnecessary dayjs import in chat-service - Added dateTimeService.now() to return dateTime - Added dateTimeService.str() to convert a dateTime to a string

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,23 +2,27 @@ import React from "react";
 import "./Message.css"
 import { services } from "../../context";
 
-function Message(props) {
-    const sentDt = services.dateTimeService.dt(props.sent_dt).format("h:mm A")
+const RAW_MESSAGE_DT_FORMAT = services.chatService.MESSAGE_DT_FORMAT;
+const DISPLAY_MESSAGE_DT_FORMAT = "h:mm A";
+
+function Message({sent_dt, sender, content}) {
+    const dateTime = services.dateTimeService.dt(sent_dt, RAW_MESSAGE_DT_FORMAT);
+    const sentDt = services.dateTimeService.str(dateTime, DISPLAY_MESSAGE_DT_FORMAT);
     return (
         <div className="message">
             <div className="message-left">
-                <div className="avatar">{props.sender[0].toUpperCase()}</div>
+                <div className="avatar">{sender[0].toUpperCase()}</div>
             </div>
             <div className="message-right">
                 <div className="message-sender">
-                    {props.sender}
+                    {sender}
                 </div>
                 <div className="message-timestamp">
                     / {sentDt} /
                 </div>
                 <br />
                 <div className="message-content">
-                    {props.content}
+                    {content}
                 </div>
             </div>
         </div>
--- src/services/chat-service.js
+++ src/services/chat-service.js
@@ -1,15 +1,15 @@
 import { Subject } from "rxjs";
-const dayjs = require("dayjs");
 
 function ChatService(dateTimeService) {
     let joinedChat$ = new Subject();
 
     const getJoinedChat$ = () => joinedChat$;
 
+    const MESSAGE_DT_FORMAT = "YYYY/MM/DD HH:mm:ss:SSS";
+
     // destination parameter can either be a username or channel name
     const prepareMessage = (type, content, sender, destination, orgName) => {
-        const date = new Date();
-        const sentDt = dateTimeService.getDtStr();
+        const sentDt = dateTimeService.str(dateTimeService.now(), MESSAGE_DT_FORMAT);
         const message = {
             type,
             sender,
@@ -29,6 +29,7 @@ function ChatService(dateTimeService) {
 
     return Object.freeze({
         getJoinedChat$,
+        MESSAGE_DT_FORMAT,
         prepareMessage,
         onUserJoinedChat,
     });
