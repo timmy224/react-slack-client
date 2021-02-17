d6bac8a1726643dd6cc2ded69c553fd249daacd3
Luis <lz24069@gmail.com>
2021-02-16 19:58:56 -0500

    add channel member code revised and tested

--- src/modules/channel/reducer.js
+++ src/modules/channel/reducer.js
@@ -7,84 +7,59 @@ const initReducer = () => {
 
 	const INITIAL_STATE = {
 		channels: {},
-		create_channel_name: "",
-		show_taken_msg: false,
-		showCreateModal: false,
-		isPrivate: false,
-		privateChannelUsers: [],
+		showCreateChannelModal: false,
 		showChannelSideBar: false,
 		channelMemberNames: [],
-		showCreateChannelModal: false,
-
 	};
 
     const reducer = (state = INITIAL_STATE, action) => {
-        const { type, payload } = action;
+		const { type, payload } = action;
+		
         switch (type) {
-            case userTypes.LOGOUT: 
-                return INITIAL_STATE;
-            case types.SET_ORG_CHANNELS: {
-                const { orgName, channels } = payload;
-                const path = ["channels", orgName]
-                return set(path, channels, state);
-            }
-            case types.ADDED_TO_CHANNEL: {
-                const { orgName, channel } = payload;
-                const path = ["channels", orgName, channel.name]
-                return set(path, channel, state);
-			}
-			case types.SET_CHANNEL_MEMBERS: {
-				const {orgName, channelName, members} = payload;
-				const path = ["channels", orgName, channelName, 'members'];
-				return set(path, members, state)
-			}
-            case types.SHOW_CREATE_CHANNEL_MODAL:
-                return {
-                    ...state,
-                    showCreateChannelModal: payload,
-                }
-            case userTypes.LOGOUT:
+			case userTypes.LOGOUT:
 				return INITIAL_STATE;
+
 			case types.SET_ORG_CHANNELS: {
-				const {orgName, channels} = payload;
+				const { orgName, channels } = payload;
 				const path = ["channels", orgName];
 				return set(path, channels, state);
 			}
-			case types.FETCH_CHANNELS:
+
+			case types.ADDED_TO_CHANNEL: {
+				const { orgName, channel } = payload;
+				const path = ["channels", orgName, channel.name];
+				return set(path, channel, state);
+			}
+
+			case types.SHOW_CREATE_CHANNEL_MODAL:
 				return {
 					...state,
-					channels: payload,
+					showCreateChannelModal: payload,
 				};
-			case types.CHANNEL_NAME_TAKEN:
-				return {
-					...state,
-					show_taken_msg: payload,
-				};
-			case types.CHANNEL_NAME_SET:
-				return {
-					...state,
-					create_channel_name: payload,
-				};
-			case types.SHOW_CREATE_MODAL:
-				return {
-					...state,
-					showCreateModal: payload,
-				};
-			case types.CREATE_PRIVATE:
-				return {
-					...state,
-					isPrivate: payload,
-				};
-			case types.PRIVATE_CHANNEL_USERS:
-				return {
-					...state,
-					privateChannelUsers: payload,
-				};
+
 			case types.SHOW_CHANNEL_SIDE_BAR:
 				return {
 					...state,
 					showChannelSideBar: payload,
 				};
+
+			case types.ADD_A_MEMBER_TO_CHANNEL: {
+				const { orgName, channelName, username } = payload;
+				const path = ["channels", orgName, channelName, "members"];
+				const members = state.channels[orgName]?.[channelName]?.members
+					? [...state.channels[orgName]?.[channelName]?.members]
+					: [];
+				members.push({ username: username });
+				return set(path, members, state);
+			}
+
+				
+			case types.SET_CHANNEL_MEMBERS: {
+				const { orgName, channelName, members } = payload;
+				const path = ["channels", orgName, channelName, "members"];
+				return set(path, members, state);
+			}
+
 			case types.FETCH_CHANNEL_MEMBER_NAMES:
 				return {
 					...state,
@@ -93,7 +68,7 @@ const initReducer = () => {
 
 			default:
 				return state;
-        }
+		}
     };
 
 	return reducer;
